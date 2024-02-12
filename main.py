48a2a5bf62cda62b5b2dc727ef9803fd64053144
vankaycee <kenechiazubuike@gmail.com>
2024-02-12 17:08:48 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,8 @@
+print("welcome, please signup")
+first_name = str(input("your first_name: "))
+last_name = str(input("your last_name: "))
+username = str(input("your username: "))
+email = (input("your email: "))
+phone_number = int(input("your phone number: "))
+
+print(f"{username} your signup was successful")
